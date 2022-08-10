8b57aeff39edf2bd8707a91c49b262afb101840b
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-08-10 14:51:53 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+session_start();
+if(!isset($_SESSION['user'])){
+    header("Location: login.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Conciergerie</title>
+</head>
+<body>
+    <script src="assets/js/app.js"></script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <title>Login Conciergerie</title>
+</head>
+<body>
+    <main>
+        <form action="connexion.php" method="post">
+            <h1>Login</h1>
+            <div>
+                <label for="username">Username:</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <section>
+                <button type="submit" name="action" value="login">Login</button>
+                <a href="register.php">Register</a>
+            </section>
+        </form>
+    </main>
+    <script src="assets/js/app.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+</html>
